b3161adf8898136e7037b58d531c7911b7d2f55a
Jonathan Dollahite <jonathandollahite@gmail.com>
2023-11-14 23:23:24 -0600

    Update dash_app_side_by_side.py

--- dash_app_side_by_side.py
+++ dash_app_side_by_side.py
@@ -10,6 +10,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # Create a Dash application
 app = Dash(__name__, external_stylesheets=external_stylesheets)\
 
+server = app.server
+
 app.title = 'Lifetime Earnings Comparison'
 
 # Read data from an Excel file for both sheets
